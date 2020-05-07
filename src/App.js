73cf5109bbaded117166a51bb7ef34f29798702e
William <williamrpelton@gmail.com>
2020-05-07 15:06:11 -0500

    Heartbeat for vendor to get activeOrders

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { Switch, Route, useHistory, Redirect } from "react-router";
+import { Switch, Route, Redirect } from "react-router";
 import { Container, Col, Spinner } from "reactstrap";
 
 import "./App.css";
 import { setBaseURL } from "./services/http";
 import { loginWithToken } from "./actions/session";
 import { getVendors } from "./actions/vendor";
+import { getActiveOrders } from "./actions/orders";
 import Navbar from "./components/Navbar";
 import VendorAuthContainer from "./screens/vendor/AuthContainer";
 import VendorOrders from "./screens/vendor/Orders";
@@ -28,6 +29,15 @@ function App(props) {
   if (authToken && !props.isLoggedIn && !props.isLoading) {
     props.loginWithToken(authToken, userType);
   }
+  useEffect(() => {
+    const { userType } = props;
+    let getActiveOrders;
+    if (userType === "vendor") {
+      getActiveOrders = setInterval(() => props.getActiveOrders(), 60000);
+    } else {
+      clearInterval(getActiveOrders);
+    }
+  }, [props.userType]);
   return (
     <Container className="App">
       <Navbar />
@@ -73,6 +83,7 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = {
   loginWithToken,
   getVendors,
+  getActiveOrders,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
